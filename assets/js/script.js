86d88ed98e0afdbd44ae5e654e2d9075a649febe
jenkidev <jenkimc@googlemail.com>
2023-03-28 04:39:25 +0000

    Fixed issue in html, Added calculate and check answer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function(){
 
     for (let button of buttons) {
         button.addEventListener("click", function(){
-            if(this.getAttribute("data-type") == "submit"){
-                alert("You clicked submit!");
+            if(this.getAttribute("data-type") === "submit"){
+                checkAnswer();
             } else {
                 let gameType= this.getAttribute("data-type");
                 runGame(gameType);
@@ -30,12 +30,31 @@ function runGame(gameType){
     }
 }
 
+/** Checks the user answer against the calculated value and returns true or false. */
 function checkAnswer(){
-    
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+    if (isCorrect) {
+        alert("You are correct :)")
+    } else {
+        alert(`Incorrect, sorry the correct answer is ${calculatedAnswer[0]}`);
+    }
+
+    runGame(calculatedAnswer[1]);
 }
 
 function calculateCorrectAnswer(){
-    
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"]
+    } else {
+        alert(`Unknown operator ${operator}`)
+        throw `Unknown operator ${operator}. Aborting.`
+    }
 }
 
 function incrementScore(){
